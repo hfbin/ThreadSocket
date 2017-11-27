afee3c8f0b56d5e9519bd728144f0c31fed180bc
HuangFuBin <1716850375@qq.com>
2017-11-27 17:17:42 +0800

    java线程安全之Executor框架及自定义线程池（十五）

---
+++ Thread/concurrent018/MyTask.java
@@ -0,0 +1,44 @@
+package cn.hfbin.concurrent018;
+
+public class MyTask implements Runnable {
+
+	private int taskId;
+	private String taskName;
+	
+	public MyTask(int taskId, String taskName){
+		this.taskId = taskId;
+		this.taskName = taskName;
+	}
+	
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("run taskId =" + this.taskId);
+			Thread.sleep(3*1000);
+			//System.out.println("end taskId =" + this.taskId);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public String toString(){
+		return Integer.toString(this.taskId);
+	}
+
+}
