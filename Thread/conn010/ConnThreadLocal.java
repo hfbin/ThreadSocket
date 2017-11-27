a170d9357aa7055742691ed0274435cb16e863c7
HuangFuBin <1716850375@qq.com>
2017-11-27 17:02:16 +0800

    java线程安全之ThreadLocaI理解（十）

---
+++ Thread/conn010/ConnThreadLocal.java
@@ -0,0 +1,41 @@
+package cn.hfbin.conn010;
+
+public class ConnThreadLocal {
+
+	public static ThreadLocal<String> th = new ThreadLocal<String>();
+	
+	public void setTh(String value){
+		th.set(value);
+	}
+	public void getTh(){
+		System.out.println(Thread.currentThread().getName() + ":" + this.th.get());
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		final ConnThreadLocal ct = new ConnThreadLocal();
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				ct.setTh("张三");
+				ct.getTh();
+			}
+		}, "t1");
+		
+		Thread t2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+					ct.getTh();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, "t2");
+		
+		t1.start();
+		t2.start();
+	}
+	
+}
