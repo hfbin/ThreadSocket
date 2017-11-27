0851c7700016239cc2ffab312098c497a79f9bdf
HuangFuBin <1716850375@qq.com>
2017-11-27 17:03:17 +0800

    java线程安全之单例&多线程（十一）

---
+++ Thread/conn011/Singletion.java
@@ -0,0 +1,13 @@
+package cn.hfbin.conn011;
+
+public class Singletion {
+	
+	private static class InnerSingletion {
+		private static Singletion single = new Singletion();
+	}
+	
+	public static Singletion getInstance(){
+		return InnerSingletion.single;
+	}
+	
+}
